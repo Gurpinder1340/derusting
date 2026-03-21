7e05e1a87b681c1589b7dd486046f131e84ecb25
Gurpinder Gill <gill.gurpinder@student.greenriver.edu>
2026-03-21 16:09:42 -0700

    added a string with 5 characters and assin to a variable

--- src/StringPractice.java
+++ src/StringPractice.java
@@ -1,6 +1,8 @@
 public class StringPractice {
   public static void main(String[] args) {
     // Create a string with at least 5 characters and assign it to a variable
+    String word = "hi";
+    System.out.println(word);
 
     // Find the length of the string
 
