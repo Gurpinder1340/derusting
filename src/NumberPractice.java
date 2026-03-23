618377a564dca8f4167ac3467b5034d2f70a661a
Gurpinder Gill <gill.gurpinder@student.greenriver.edu>
2026-03-22 17:33:44 -0700

    num practice with float, int and modulo operator

--- src/NumberPractice.java
+++ src/NumberPractice.java
@@ -1,10 +1,15 @@
 public class NumberPractice {
   public static void main(String args[]) {
     // Create a float with a negative value and assign it to a variable
+    float negativeFloat = -7.7f;
+    System.out.println("Negative float: " + negativeFloat);
 
     // Create an int with a positive value and assign it to a variable
+    int number = 7;
 
     // Use the modulo % operator to find the remainder when the int is divided by 3
+  int remainder = number % 3;
+  System.out.println("Remainder: " + remainder);
 
     // Use the modulo % operator to determine whether the number is even
     // (A number is even if it has a remainder of zero when divided by 2)
