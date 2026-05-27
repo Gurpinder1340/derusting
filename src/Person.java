e54c36141a20496ff009d753753744a71ebea6f1
Gurpinder Gill <gill.gurpinder@student.greenriver.edu>
2026-05-27 14:03:23 -0700

    Completed Person.java

--- src/Person.java
+++ src/Person.java
@@ -6,14 +6,25 @@
 public class Person {
   // Declare a public String instance variable for the name of the person
   // Declare a private int instance variable for the age of the person
+    public String name;
 
+    private int age;
 
   // Create a constructor that takes the name and age of the person
   // and assigns it to the instance variables
 
+  public Person(String name, int age ) {
+    this.name = name;
+    this.age = age;
+  }
+
+
 
   // Create a toString method that gives the name and age of the person
-
+@Override
+ public String toString() {
+        return "Person{name=" + name + ", age=" + age + "}";
+}
 
   // Implement the below public instance method "birthYear"
   // There should NOT be any print statement in this method.
@@ -28,25 +39,35 @@ public class Person {
    * @return The year the person was born
    */
   // (create the instance method here)
+  public int birthYear(int currentYear) {
+    return currentYear - age;
+  }
 
 
   public static void main(String[] args) {
     // Create an instance of Person
+    Person bob = new Person("Bob", 25);
 
     // Create another instance of Person with a different name and age and
     // assign it to a different variable
+    Person john = new Person("John", 24);
 
     // Print the first person
+    System.out.println(bob);
 
     // Print the second person
+    System.out.println(john);
 
     // Get the name of the first person and store it in a local variable
+    String bobFirstName = bob.name;
 
     // Using the birthYear method, get the birth year of the first person
     // and store it in a local variable. Input the actual current year (e.g. 2025)
     // as the argument.
+    int JohnBirthYear = john.birthYear(2026);
 
     // In a separate statement, print the local variable holding the birth year.
+    System.out.println(JohnBirthYear);
 
     /**
      * Terminology!
